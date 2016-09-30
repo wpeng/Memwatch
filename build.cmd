19f8cb3ed68fe4401b88498f55e42463f0397138
wpeng <zju.pengwei@gmail.com>
2016-09-30 15:06:30 +0800

    Turn line terminator of .cmd to \r\n

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-ROOT=$~dp0
-ndk-build NDK_PROJECT_PATH=%ROOT% NDK_APPLICATION_MK=%ROOT%/Application.mk APP_BUILD_SCRIPT=%ROOT%/Android.mk 
+ROOT=%~dp0
+ndk-build NDK_PROJECT_PATH=%ROOT% NDK_APPLICATION_MK=%ROOT%/Application.mk APP_BUILD_SCRIPT=%ROOT%/Andrnoid.mk 
